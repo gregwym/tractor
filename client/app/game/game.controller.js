7810d8f3bfe1a8c5b69ca5b79d3f6daed8b1ef1a
Greg Wang <gregwym@gmail.com>
2014-08-03 17:46:05 -0400

    Refactor game.controller.

--- client/app/game/game.controller.js
+++ client/app/game/game.controller.js
@@ -2,31 +2,45 @@
 
 angular.module('tractorApp')
   .controller('GameCtrl', function ($scope, $http, $stateParams, socket, Auth) {
+    var off = [];
+
     var gameId = $stateParams.id;
     var gameUrl = '/api/games/' + gameId;
     var currentUser = Auth.getCurrentUser();
 
-    $http.get(gameUrl).success(function(game) {
-      $scope.game = game;
-      socket.syncItemUpdates('game', $scope.game, function(event) {
-        if (event === 'deleted') {
-          // Game ended
-          $scope.game = null;
-        }
-      });
+    var init = function() {
 
-      $http.put(gameUrl, {
-        join: currentUser
-      });
-    });
+      $http.get(gameUrl).success(function(game) {
+        $scope.game = game;
+        socket.syncItemUpdates('game', $scope.game, function(event) {
+          if (event === 'deleted') {
+            // Game ended
+            $scope.game = null;
+          }
+        });
 
-    $scope.$on('$destroy', function () {
+        $http.put(gameUrl, {
+          join: currentUser
+        });
+      });
+    };
+
+    var destructor = function() {
       if ($scope.game) {
         socket.unsyncItemUpdates('game', $scope.game);
       }
-
       $http.put(gameUrl, {
         quit: currentUser
       });
-    });
+
+      _.each(off, function(unbind) {
+        unbind();
+      });
+      off = null;
+    };
+
+    off.push($scope.$on('$destroy', function () {
+      destructor();
+    }));
+    init();
   });
